a2d08a8ffbaf325a9b0521f0341dfb9ffad500bc
Justin Schnurer <JSchnurer@slb.com>
2020-10-08 16:02:55 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mm-tools/precache-manifest.84375781ae81998259538d06e6d13b6e.js"
+  "/mm-tools/precache-manifest.a25d34036d44bb52a1980367cb6368cf.js"
 );
 
 self.addEventListener('message', (event) => {
