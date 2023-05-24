55a2cf1ec9f5e132a753f2ace7e17060cb6a6824
Justin Schnurer <JSchnurer@slb.com>
2023-05-24 15:34:52 -0400

    updated MM6 map POIs to show master/expert border

--- src/components/maps/interactive/icons.ts
+++ src/components/maps/interactive/icons.ts
@@ -1,53 +1,74 @@
 import { Icon } from "leaflet";
 
+let iconMappings = [
+  { name: "spear", iconUrl: "/mm-tools/images/leaflet/icons/arrowhead.svg", },
+  { name: "dark", iconUrl: "/mm-tools/images/leaflet/icons/moon.svg", },
+  { name: "identify", iconUrl: "/mm-tools/images/leaflet/icons/magnifying-glass.svg", },
+  { name: "dagger", iconUrl: "/mm-tools/images/leaflet/icons/stiletto.svg", },
+  { name: "spirit", iconUrl: "/mm-tools/images/leaflet/icons/aura.svg", },
+  { name: "perception", iconUrl: "/mm-tools/images/leaflet/icons/eye-target.svg", },
+  { name: "meditation", iconUrl: "/mm-tools/images/leaflet/icons/meditation.svg", },
+  { name: "earth", iconUrl: "/mm-tools/images/leaflet/icons/stone-pile.svg", },
+  { name: "repair", iconUrl: "/mm-tools/images/leaflet/icons/auto-repair.svg", },
+  { name: "fire", iconUrl: "/mm-tools/images/leaflet/icons/flame.svg", },
+  { name: "bodyBuilding", iconUrl: "/mm-tools/images/leaflet/icons/muscle-up.svg", },
+  { name: "light", iconUrl: "/mm-tools/images/leaflet/icons/sun.svg", },
+  { name: "bank", iconUrl: "/mm-tools/images/leaflet/icons/bank.svg", },
+  { name: "mace", iconUrl: "/mm-tools/images/leaflet/icons/flanged-mace.svg", },
+  { name: "body", iconUrl: "/mm-tools/images/leaflet/icons/person.svg", },
+  { name: "axe", iconUrl: "/mm-tools/images/leaflet/icons/battle-axe.svg", },
+  { name: "temple", iconUrl: "/mm-tools/images/leaflet/icons/greek-temple.svg", },
+  { name: "mind", iconUrl: "/mm-tools/images/leaflet/icons/psychic-waves.svg", },
+  { name: "air", iconUrl: "/mm-tools/images/leaflet/icons/tornado.svg", },
+  { name: "sword", iconUrl: "/mm-tools/images/leaflet/icons/broadsword.svg", },
+  { name: "bow", iconUrl: "/mm-tools/images/leaflet/icons/high-shot.svg", },
+  { name: "diplomacy", iconUrl: "/mm-tools/images/leaflet/icons/public-speaker.svg", },
+  { name: "quest", iconUrl: "/mm-tools/images/leaflet/icons/uncertainty.svg", },
+  { name: "merchant", iconUrl: "/mm-tools/images/leaflet/icons/cash.svg", },
+  { name: "coach", iconUrl: "/mm-tools/images/leaflet/icons/horse-head.svg", },
+  { name: "blaster", iconUrl: "/mm-tools/images/leaflet/icons/ray-gun.svg", },
+  { name: "fountain", iconUrl: "/mm-tools/images/leaflet/icons/water-fountain.svg", },
+  { name: "chain", iconUrl: "/mm-tools/images/leaflet/icons/chain-mail.svg", },
+  { name: "house", iconUrl: "/mm-tools/images/leaflet/icons/house.svg", },
+  { name: "learning", iconUrl: "/mm-tools/images/leaflet/icons/read.svg", },
+  { name: "staff", iconUrl: "/mm-tools/images/leaflet/icons/wizard-staff.svg", },
+  { name: "cult", iconUrl: "/mm-tools/images/leaflet/icons/church.svg", },
+  { name: "shield", iconUrl: "/mm-tools/images/leaflet/icons/shield.svg", },
+  { name: "disarm", iconUrl: "/mm-tools/images/leaflet/icons/wolf-trap.svg", },
+  { name: "water", iconUrl: "/mm-tools/images/leaflet/icons/droplets.svg", },
+  { name: "info", iconUrl: "/mm-tools/images/leaflet/icons/info.svg", },
+  { name: "shop", iconUrl: "/mm-tools/images/leaflet/icons/shop.svg", },
+  { name: "dungeon", iconUrl: "/mm-tools/images/leaflet/icons/3d-stairs.svg", },
+  { name: "leather", iconUrl: "/mm-tools/images/leaflet/icons/leather-armor.svg", },
+  { name: "plate", iconUrl: "/mm-tools/images/leaflet/icons/shoulder-armor.svg", },
+  { name: "obelisk", iconUrl: "/mm-tools/images/leaflet/icons/obelisk.svg", },
+  { name: "inn", iconUrl: "/mm-tools/images/leaflet/icons/beer-stein.svg", },
+  { name: "docks", iconUrl: "/mm-tools/images/leaflet/icons/anchor.svg", },
+  { name: "training", iconUrl: "/mm-tools/images/leaflet/icons/crossed-swords.svg", },
+  { name: "townHall", iconUrl: "/mm-tools/images/leaflet/icons/congress.svg", },
+];
+
 let icons: {
   [index: string]: Icon
-} = {
-  spear: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/arrowhead.svg", iconSize: [32, 32], className: "marker" }),
-  dark: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/moon.svg", iconSize: [32, 32], className: "marker" }),
-  identify: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/magnifying-glass.svg", iconSize: [32, 32], className: "marker" }),
-  dagger: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/stiletto.svg", iconSize: [32, 32], className: "marker" }),
-  spirit: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/aura.svg", iconSize: [32, 32], className: "marker" }),
-  perception: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/eye-target.svg", iconSize: [32, 32], className: "marker" }),
-  meditation: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/meditation.svg", iconSize: [32, 32], className: "marker" }),
-  earth: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/stone-pile.svg", iconSize: [32, 32], className: "marker" }),
-  repair: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/auto-repair.svg", iconSize: [32, 32], className: "marker" }),
-  fire: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/flame.svg", iconSize: [32, 32], className: "marker" }),
-  bodyBuilding: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/muscle-up.svg", iconSize: [32, 32], className: "marker" }),
-  light: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/sun.svg", iconSize: [32, 32], className: "marker" }),
-  bank: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/bank.svg", iconSize: [32, 32], className: "marker" }),
-  mace: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/flanged-mace.svg", iconSize: [32, 32], className: "marker" }),
-  body: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/person.svg", iconSize: [32, 32], className: "marker" }),
-  axe: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/battle-axe.svg", iconSize: [32, 32], className: "marker" }),
-  temple: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/greek-temple.svg", iconSize: [32, 32], className: "marker" }),
-  mind: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/psychic-waves.svg", iconSize: [32, 32], className: "marker" }),
-  air: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/tornado.svg", iconSize: [32, 32], className: "marker" }),
-  sword: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/broadsword.svg", iconSize: [32, 32], className: "marker" }),
-  bow: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/high-shot.svg", iconSize: [32, 32], className: "marker" }),
-  diplomacy: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/public-speaker.svg", iconSize: [32, 32], className: "marker" }),
-  quest: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/uncertainty.svg", iconSize: [32, 32], className: "marker" }),
-  merchant: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/cash.svg", iconSize: [32, 32], className: "marker" }),
-  coach: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/horse-head.svg", iconSize: [32, 32], className: "marker" }),
-  blaster: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/ray-gun.svg", iconSize: [32, 32], className: "marker" }),
-  fountain: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/water-fountain.svg", iconSize: [32, 32], className: "marker" }),
-  chain: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/chain-mail.svg", iconSize: [32, 32], className: "marker" }),
-  house: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/house.svg", iconSize: [32, 32], className: "marker" }),
-  learning: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/read.svg", iconSize: [32, 32], className: "marker" }),
-  staff: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/wizard-staff.svg", iconSize: [32, 32], className: "marker" }),
-  cult: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/church.svg", iconSize: [32, 32], className: "marker" }),
-  shield: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/shield.svg", iconSize: [32, 32], className: "marker" }),
-  disarm: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/wolf-trap.svg", iconSize: [32, 32], className: "marker" }),
-  water: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/droplets.svg", iconSize: [32, 32], className: "marker" }),
-  info: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/info.svg", iconSize: [32, 32], className: "marker" }),
-  shop: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/shop.svg", iconSize: [32, 32], className: "marker" }),
-  dungeon: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/3d-stairs.svg", iconSize: [32, 32], className: "marker" }),
-  leather: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/leather-armor.svg", iconSize: [32, 32], className: "marker" }),
-  plate: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/shoulder-armor.svg", iconSize: [32, 32], className: "marker" }),
-  obelisk: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/obelisk.svg", iconSize: [32, 32], className: "marker" }),
-  inn: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/beer-stein.svg", iconSize: [32, 32], className: "marker" }),
-  docks: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/anchor.svg", iconSize: [32, 32], className: "marker" }),
-  training: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/crossed-swords.svg", iconSize: [32, 32], className: "marker" }),
-  townHall: new Icon({ iconUrl: "/mm-tools/images/leaflet/icons/congress.svg", iconSize: [32, 32], className: "marker" }),
-};
+} = {};
+
+let duplicates = ["", "Expert", "Master"];
+
+iconMappings.forEach(iconMapping => {
+  duplicates.forEach(dupeSuffix => {
+    icons[iconMapping.name + dupeSuffix] = new Icon({
+      iconUrl: iconMapping.iconUrl,
+      iconSize: [32, 32],
+      className: `marker ${dupeSuffix}`,
+    });
+
+    if (iconMapping.name === "blaster") {
+      console.log(iconMapping.name + dupeSuffix);
+      console.log(icons[iconMapping.name + dupeSuffix]);
+    }
+  });
+});
+
+(window as any).icons = icons;
 
 export default icons;
